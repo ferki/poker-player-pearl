4179a50fce6911011adb14084a4174c6ec224912
Ferenc Erki <ferki@adjust.com>
2016-04-30 15:32:39 +0200

    factor out betting

--- Player.pm
+++ Player.pm
@@ -3,15 +3,18 @@ package Player;
 use strict;
 use warnings;
 
-our $VERSION = '0.0.2';
+our $VERSION = '0.0.3';
 
 sub new {
-    my ( $class, $args ) = @_;
-    return bless \$args, $class;
+    my $class = shift;
+    my $args  = {};
+    return bless $args, $class;
 }
 
 sub bet_request {
-    return 0;
+    my $self = shift;
+    $self->{game_state} = shift;
+    return $self->get_bet();
 }
 
 sub check { }
@@ -21,6 +24,10 @@ sub showdown { }
 sub version {
     my $self = shift;
     return $self->VERSION;
+}
+
+sub get_bet {
+    return 1000;
 }
 
 1;
